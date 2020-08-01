6904776d878ac74e90a296ccebca1a9f33cae38c
Augustine Emeka <c3ono32@yahoo.com>
2020-08-01 05:55:44 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,9 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(bodyPaser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes);
