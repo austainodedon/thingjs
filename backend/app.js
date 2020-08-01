6265f4aa27a05e28af6058b2a370a8563da952d7
Austaino <c3ono32@yahoo.com.com>
2020-08-01 00:50:53 -0700

    Initial Project Commit

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const userRoutes = require('./routes/user');
+const stuffRoutes = require('./routes/stuff');
+const mongoose = require('mongoose');
+
+const path = require('path');
+
+mongoose
+  .connect(
+    'mongodb+srv://ekene:XjbFRgkxy5fszTsA@cluster0-t35lk.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+//app.use(bodyPaser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+app.use(bodyPaser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
